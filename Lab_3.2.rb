6b1ae0e663e13a03ab01e62c9fec3dcbc52e772b
Марина Низий <43046245+MarNiziy@users.noreply.github.com>
2021-04-04 21:15:08 +0300

    Update Lab_3.2.rb

--- Lab_3.2.rb
+++ Lab_3.2.rb
@@ -3,51 +3,50 @@ def menu
 	if File.exists?('D:\balance.txt') == true
 		file_data = File.read('D:\balance.txt')
 		@f = file_data.to_f
-    	else
-    	@f = 100.0
-    end
+    	        else
+    	        @f = 100.0
+        end
 	puts "Ваш текущий баланс: #{@f}"
 
 	loop do
-      	print 'Выберите действие для работы с балансом (D/W/B/Q):'
-      	act = gets.chomp.upcase || act = gets.chomp.downcase
+      	   print 'Выберите действие для работы с балансом (D/W/B/Q):'
+      	   act = gets.chomp.upcase || act = gets.chomp.downcase
       	
-      	if act == 'D' 
-      	deposit
-      	elsif act == 'W'
+      	   if act == 'D' 
+      	   deposit
+      	   elsif act == 'W'
       		withdraw
       		elsif act == 'B'
-      			balance
-      			elsif act == 'Q'
-      			File.open('D:\balance.txt', "w") { |f| f.write "#{@f}" }
-
-      			break
+      		     balance
+      		     elsif act == 'Q'
+      		     File.open('D:\balance.txt', "w") { |f| f.write "#{@f}" }
+      		     break
       		end
-      	end
-    end
+      	   end
+	end
       
-    def deposit
-    	print 'Введите сумму для депозита:'
-    	sum = gets.to_f
-    	if sum > 0
-    	@f = sum + @f
+        def deposit
+    	    print 'Введите сумму для депозита:'
+    	    sum = gets.to_f
+    	    if sum > 0
+    	    @f = sum + @f
     		else puts "Ваша сумма должна быть больше нуля!"
-    	end	
-    	puts "Ваш баланс: #{@f}"
-    end
+    	    end	
+    	    puts "Ваш баланс: #{@f}"
+        end
 
-    def withdraw
-    	print 'Введите сумму для снятия:'
-    	sum = gets.to_f
-    	if sum > 0 && sum <= @f
-    	@f = @f - sum
+    	def withdraw
+    	    print 'Введите сумму для снятия:'
+    	    sum = gets.to_f
+    	    if sum > 0 && sum <= @f
+    	    @f = @f - sum
     		else puts "Ваша сумма не корректна!"
-    	end	
-    	puts "Ваш баланс: #{@f}"
-    end
+    	    end	
+    	    puts "Ваш баланс: #{@f}"
+    	end
 
-    def balance
-    	puts "Ваш текущий баланс: #{@f}"
-    end
+       def balance
+    	   puts "Ваш текущий баланс: #{@f}"
+       end
 
 menu
