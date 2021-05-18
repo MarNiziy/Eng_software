eda478d4a04c5b1621a241d8aa4f79f9efece83c
Марина Низий <43046245+MarNiziy@users.noreply.github.com>
2021-05-18 15:03:19 +0300

    Update Lab_5.rb

--- Lab_5.rb
+++ Lab_5.rb
@@ -1,80 +1,61 @@
 require 'socket'
 
-server = TCPServer.new 3000
+server = TCPServer.new 6389
 
 class CashMachine
-	def init	
-	 if File.exists?('D:\balance.txt') == true
-		  file_data = File.read('D:\balance.txt')
-		  @f = file_data.to_f
-      else
-      @f = 100.0
-    end
-	 puts "Ваш текущий баланс: #{@f}"	
-	
-	 loop do
-      print 'Выберите действие для работы с балансом (D/W/B/Q):'
-      act = gets.chomp.upcase || act = gets.chomp.downcase
-      	
-        if act == 'D' 
-          self.deposit
-          elsif act == 'W'
-          self.withdraw
-          elsif act == 'B'
-          self.balance
-          elsif act == 'Q'
-          File.open('D:\balance.txt', "w") { |f| f.write "#{@f}" }
-          break
-        end
-    end
-  end
-    
-  def deposit
-    print 'Введите сумму для депозита:'
-    sum = gets.to_f
+  
+  $cash = (File.exists?('D:\balance.txt') ? File.read('D:\balance.txt') : 100.0).to_f
+  
+  def deposit(sum)
     if sum > 0
-    @f = sum + @f
-    	else puts "Ваша сумма должна быть больше нуля!"
+    $cash = sum + $cash
+    	else 'Your amount must be greater than zero!'		
     end	
-    puts "Ваш баланс: #{@f}"
+    "Your balance1: #{$cash}"
   end
 
-  def withdraw
-    print 'Введите сумму для снятия:'
-    sum = gets.to_f
-    if sum > 0 && sum <= @f
-    @f = @f - sum
-    	else puts "Ваша сумма не корректна!"
+  def withdraw(sum)
+    if sum > 0 && sum <= $cash
+    $cash = $cash - sum
+    	else 'Your amount is not correct!'
     end	
-    puts "Ваш баланс: #{@f}"
+    "Your balance2: #{$cash}"
   end
 
   def balance
-    puts "Ваш текущий баланс: #{@f}"
+    "Your balance: #{$cash}"
   end  
 end
 
-сash = CashMachine.new
+while (connection = server.accept)
+  cmachine = CashMachine.new
 
-while connection = server.accept
+  puts('Сервер запущен')
   request = connection.gets
-  method, full_path = request.split(' ')
-  path = full_path.split('?')[0]
-  method = full_path.split('?')[1]
-  
-  connection.print("HTTP/1.1 #{status}\r\n")
+  pathway, full_path = request.split(' ')
+  path = full_path.split('/')[1]
+ 
+  next unless pathway == 'GET'
+  if full_path.split('/')[1].include?('?')
+    method = path.split('?')[0]
+    value = path.split('?')[1].split('=')[1].to_f
+  end
+
+  connection.print "HTTP/1.1 200\r\n"
   connection.print "Content-Type: text/html\r\n"
   connection.print "\r\n"
-  connection.print сash.balance if path == 'balance'
-
+  
   next if value.nil?
   connection.print case method
-        when 'deposit'
-            сash.deposit(value)
-        when 'withdraw'
-            сash.withdraw(value)
-        else 'error'
-    end
-
-  connection.close
+                   when 'deposit'
+                     cmachine.deposit(value)
+                   when 'withdraw'
+                     cmachine.withdraw(value)
+                   when 'balance'
+                     cmachine.balance
+                   else
+                     'Oops error...'
+                   end
+  File.open('D:\balance.txt', "w") { |f| f.write "#{$cash}" }  
+  connection.close 
 end
