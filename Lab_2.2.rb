06a859079b2262a673410b443aa350dacfd7b9ea
Marina Niziy <mary.nizy@yandex.ru>
2024-11-11 18:55:51 +0300

    Update Lab_2.2.rb

--- Lab_2.2.rb
+++ Lab_2.2.rb
@@ -1,17 +1,17 @@
 def pr4 
-  puts "Сколько покемонов добавить?"
+	puts "Сколько покемонов добавить?"
   k = gets.to_i
 
   pokemon = []
   i = 0
+
   k.times do 
-	puts "Введите имя покемона"
-	name = gets.chomp
-	puts "Введите цвет покемона"
-	color = gets.chomp
-    pokemon.append({'name '=> name, 'color ' => color})
+		puts "Введите имя покемона"
+		name = gets.chomp
+		puts "Введите цвет покемона"
+		color = gets.chomp
+	  pokemon.append({'name '=> name, 'color ' => color})
   end
-  
   puts pokemon
 end
 
