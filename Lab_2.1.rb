104a373caf3428d357342c0cbe8e0b3b5d2bc8b0
Marina Niziy <mary.nizy@yandex.ru>
2024-11-11 18:52:07 +0300

    Update Lab_2.1.rb

--- Lab_2.1.rb
+++ Lab_2.1.rb
@@ -3,11 +3,11 @@ word = gets.split(" ")
 w = word[1].downcase 
 
 def pr3(w)
-if  (w[w.length-1] == "s" && w[w.length-2] == "c")
-  puts 2 ** w.length
-else 
-  puts w.reverse
-end
+  if  (w[w.length-1] == "s" && w[w.length-2] == "c")
+    puts 2 ** w.length
+  else 
+    puts w.reverse
+  end
 end
 
 pr3(w)
