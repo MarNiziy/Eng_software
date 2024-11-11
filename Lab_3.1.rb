711bdbbcd42626f680c2b437d709aad92afb151f
Marina Niziy <mary.nizy@yandex.ru>
2024-11-11 19:01:26 +0300

    Update Lab_3.1.rb

--- Lab_3.1.rb
+++ Lab_3.1.rb
@@ -1,21 +1,22 @@
 def files
-   
 	require 'fileutils'
-	files = FileUtils.compare_file('D:\students.txt','D:\results.txt')
-    @f_comp = files
 
-    File.open('D:\students.txt', 'r') do |file|
-    @file_data = file.readlines.map(&:chomp)
-    end
+	files = FileUtils.compare_file('D:\students.txt','D:\results.txt')
+	@f_comp = files
+
+	File.open('D:\students.txt', 'r') do |file|
+		@file_data = file.readlines.map(&:chomp)
+	end
    
-    5.times do |x|
-   	 puts "Введите возраст студента:"
-   	 age = gets.to_i    
-     	if age == @file_data[x].split(" ")[2].to_i 
-     	File.write('D:\results.txt',@file_data[x].split("/n"), mode:"a")
-        elsif age == -1 || @f_comp == true
-        break
-        end
-    end
+	5.times do |x|
+		puts "Введите возраст студента:"
+		age = gets.to_i    
+		if age == @file_data[x].split(" ")[2].to_i 
+			File.write('D:\results.txt',@file_data[x].split("/n"), mode:"a")
+		elsif age == -1 || @f_comp == true
+			break
+		end
+	end
 end
+
 files
