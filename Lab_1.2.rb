379cc1b01cdc6c1266203c734d31a8b5d5a1f1d4
Marina Niziy <mary.nizy@yandex.ru>
2024-11-11 18:49:50 +0300

    Update Lab_1.2.rb

--- Lab_1.2.rb
+++ Lab_1.2.rb
@@ -1,4 +1,3 @@
-
 def pr2 
    puts "Введите 1 число"
    first_value = gets.to_i
@@ -11,12 +10,12 @@ def pr2
    
    puts "Введите 4 число"
    fourth_value = gets.to_i
-  
+
     if (first_value == 20) || (second_value == 20) || (third_value == 20) || (fourth_value == 20)
-        puts 20
+      puts 20
     else
        puts (first_value + second_value + third_value + fourth_value)
     end
 end
-   
+
 pr2
