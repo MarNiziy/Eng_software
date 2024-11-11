675d72aa5fc6f6829d532937cd913c84371e5a66
Marina Niziy <mary.nizy@yandex.ru>
2024-11-11 18:49:09 +0300

    Update Lab_1.1.rb

--- Lab_1.1.rb
+++ Lab_1.1.rb
@@ -1,4 +1,3 @@
-
 if (Gem.win_platform?)
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
   Encoding.default_internal = __ENCODING__
@@ -18,15 +17,11 @@ puts "Введите ваш возраст"
 age = gets.to_i
 
 def gree1(name,surname,age)
-    
-    if age < 18
-  puts "Привет,#{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не поздно:)"
-    else
-  puts "Привет,#{name} #{surname}. Самое время заняться делом!"
-    end
+  if age < 18
+    puts "Привет,#{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не поздно:)"
+  else
+    puts "Привет,#{name} #{surname}. Самое время заняться делом!"
+  end
 end
 
 gree1(name,surname,age)
-
-
-
