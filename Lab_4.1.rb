598a05bb634bb4df32505904928a805180db3121
Marina Niziy <mary.nizy@yandex.ru>
2024-11-11 19:23:33 +0300

    Update Lab_4.1.rb

--- Lab_4.1.rb
+++ Lab_4.1.rb
@@ -1,37 +1,39 @@
 class CashMachine
-
-	def init	
-	 if File.exists?('D:\balance.txt') == true
-		  file_data = File.read('D:\balance.txt')
-		  @f = file_data.to_f
+  def init	
+    if File.exists?('D:\balance.txt') == true
+      file_data = File.read('D:\balance.txt')
+      @f = file_data.to_f
       else
       @f = 100.0
     end
-	 puts "Ваш текущий баланс: #{@f}"	
-	
-	 loop do
+    
+    puts "Ваш текущий баланс: #{@f}"	
+
+    loop do
       print 'Выберите действие для работы с балансом (D/W/B/Q):'
       act = gets.chomp.upcase || act = gets.chomp.downcase
-      	
-        if act == 'D' 
-          self.deposit
-          elsif act == 'W'
-          self.withdraw
-          elsif act == 'B'
-          self.balance
-          elsif act == 'Q'
-          File.open('D:\balance.txt', "w") { |f| f.write "#{@f}" }
-          break
-        end
+
+      if act == 'D' 
+        self.deposit
+      elsif act == 'W'
+        self.withdraw
+      elsif act == 'B'
+        self.balance
+      elsif act == 'Q'
+        File.open('D:\balance.txt', "w") { |f| f.write "#{@f}" }
+        break
+      end
     end
   end
-    
+
   def deposit
     print 'Введите сумму для депозита:'
     sum = gets.to_f
+
     if sum > 0
-    @f = sum + @f
-    	else puts "Ваша сумма должна быть больше нуля!"
+      @f = sum + @f
+    else 
+      puts "Ваша сумма должна быть больше нуля!"
     end	
     puts "Ваш баланс: #{@f}"
   end
@@ -39,9 +41,11 @@ class CashMachine
   def withdraw
     print 'Введите сумму для снятия:'
     sum = gets.to_f
+
     if sum > 0 && sum <= @f
-    @f = @f - sum
-    	else puts "Ваша сумма не корректна!"
+      @f = @f - sum
+    else 
+      puts "Ваша сумма не корректна!"
     end	
     puts "Ваш баланс: #{@f}"
   end
@@ -49,7 +53,6 @@ class CashMachine
   def balance
     puts "Ваш текущий баланс: #{@f}"
   end
-  
 end
 
 balance = CashMachine.new
